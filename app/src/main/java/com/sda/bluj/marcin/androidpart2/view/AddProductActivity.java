b6774a45ad6f0c0974b95a9c41e58c94309109b1
marcinbluj <marcinbluj21@gmail.com>
2017-02-21 15:53:25 +0100

    changes

--- app/src/main/java/com/sda/bluj/marcin/androidpart2/view/AddProductActivity.java
+++ app/src/main/java/com/sda/bluj/marcin/androidpart2/view/AddProductActivity.java
@@ -1,26 +1,31 @@
 package com.sda.bluj.marcin.androidpart2.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.RadioButton;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.sda.bluj.marcin.androidpart2.R;
+import com.sda.bluj.marcin.androidpart2.model.Product;
+import com.sda.bluj.marcin.androidpart2.view.widget.ProductCardView;
+import com.sda.bluj.marcin.androidpart2.view.widget.ProductDetailsActivity;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by RENT on 2017-02-20.
  */
 
-public class AddProductActivity extends AppCompatActivity {
-    @BindView(R.id.credit_radio_button)
-    RadioButton credit;
-
-    @BindView(R.id.money_radio_button)
-    RadioButton money;
+public class AddProductActivity extends AppCompatActivity implements ProductCardView.ProductCardViewInterface{
+//    @BindView(R.id.credit_radio_button)
+//    RadioButton credit;
+//
+//    @BindView(R.id.money_radio_button)
+//    RadioButton money;
 
 
     @Override
@@ -30,7 +35,26 @@ public class AddProductActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    public void onAddProductClicked(View view) {
+//    @OnClick(R.id.add_product_button)
+//    public void onAddProductClicked(View view) {
+//        ProductCardView productCardView = new ProductCardView(getApplicationContext());
+//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams
+//                (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//
+//        productCardView.setLayoutParams(params);
+//
+//        Product product = new Product(11, "Aaa", 5, R.drawable.lobelia);
+//        product.setmDescription("aaa");
+//        productCardView.bindTo(product, this);
+//
+//        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.line1);
+//        linearLayout.addView(productCardView);
+//    }
 
+    @Override
+    public void onProductClicked(Product product) {
+        Intent intent = new Intent(this, ProductDetailsActivity.class);
+        intent.putExtra(ProductDetailsActivity.INTENT_PRODUCT_ID, product.getmId());
+        startActivity(intent);
     }
 }
